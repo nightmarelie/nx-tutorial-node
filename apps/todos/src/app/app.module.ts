6aa788dc06ed206d13a9d65a2317961416f2057a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-30 20:40:27 -0600

    feat: create todo service

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoService } from './todo/todo.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
