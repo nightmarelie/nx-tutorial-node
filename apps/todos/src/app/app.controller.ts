4d324ab7fc8f3df658c1da4715ed34b3e45b7aeb
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-30 20:46:34 -0600

    feat: inject service to our controller

--- apps/todos/src/app/app.controller.ts
+++ apps/todos/src/app/app.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { TodosService } from './todo/todo.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private todosService: TodosService
+  ) {}
+
+  @Get('api')
+  getData() {
+    return this.todosService.getTodos();
+  }
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  @Render('index')
+  root() {
+    return {
+      todos: this.getData(),
+    };
   }
 }
